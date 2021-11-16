c9df01a19e537bda8924204e144be36e18bccb00
Siddharatha.Dhumale <shdhumale@gmail.com>
2021-11-16 14:54:21 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
-  template: `<re-captcha (resolved)="resolved($event)" siteKey="YOUR_SIKE_KEY"></re-captcha>`,
+  template: `<re-captcha (resolved)="resolved($event)" siteKey="YOUR_SITE_KEY"></re-captcha>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
